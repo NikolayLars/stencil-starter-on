f0e527598e3391973f79c0c80630f618aab2525a
Melanie Bildhoff <mel.bildhoff.20@lehre.mosbach.dhbw.de>
2021-04-15 17:44:28 +0200

    Melly-component

--- src/components.d.ts
+++ src/components.d.ts
@@ -6,9 +6,15 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
+    interface AdrianComponent {
+    }
     interface ExampleComponent {
         "exampleProp": string;
         "exampleToUpperCase": () => Promise<void>;
+    }
+    interface JonaComponent {
+    }
+    interface JuliaComponent {
     }
     interface MyComponent {
         /**
@@ -26,11 +32,29 @@ export namespace Components {
     }
 }
 declare global {
+    interface HTMLAdrianComponentElement extends Components.AdrianComponent, HTMLStencilElement {
+    }
+    var HTMLAdrianComponentElement: {
+        prototype: HTMLAdrianComponentElement;
+        new (): HTMLAdrianComponentElement;
+    };
     interface HTMLExampleComponentElement extends Components.ExampleComponent, HTMLStencilElement {
     }
     var HTMLExampleComponentElement: {
         prototype: HTMLExampleComponentElement;
         new (): HTMLExampleComponentElement;
+    };
+    interface HTMLJonaComponentElement extends Components.JonaComponent, HTMLStencilElement {
+    }
+    var HTMLJonaComponentElement: {
+        prototype: HTMLJonaComponentElement;
+        new (): HTMLJonaComponentElement;
+    };
+    interface HTMLJuliaComponentElement extends Components.JuliaComponent, HTMLStencilElement {
+    }
+    var HTMLJuliaComponentElement: {
+        prototype: HTMLJuliaComponentElement;
+        new (): HTMLJuliaComponentElement;
     };
     interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
     }
@@ -39,14 +63,23 @@ declare global {
         new (): HTMLMyComponentElement;
     };
     interface HTMLElementTagNameMap {
+        "adrian-component": HTMLAdrianComponentElement;
         "example-component": HTMLExampleComponentElement;
+        "jona-component": HTMLJonaComponentElement;
+        "julia-component": HTMLJuliaComponentElement;
         "my-component": HTMLMyComponentElement;
     }
 }
 declare namespace LocalJSX {
+    interface AdrianComponent {
+    }
     interface ExampleComponent {
         "exampleProp"?: string;
         "onExampleEvent"?: (event: CustomEvent<string>) => void;
+    }
+    interface JonaComponent {
+    }
+    interface JuliaComponent {
     }
     interface MyComponent {
         /**
@@ -63,7 +96,10 @@ declare namespace LocalJSX {
         "middle"?: string;
     }
     interface IntrinsicElements {
+        "adrian-component": AdrianComponent;
         "example-component": ExampleComponent;
+        "jona-component": JonaComponent;
+        "julia-component": JuliaComponent;
         "my-component": MyComponent;
     }
 }
@@ -71,7 +107,10 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
+            "adrian-component": LocalJSX.AdrianComponent & JSXBase.HTMLAttributes<HTMLAdrianComponentElement>;
             "example-component": LocalJSX.ExampleComponent & JSXBase.HTMLAttributes<HTMLExampleComponentElement>;
+            "jona-component": LocalJSX.JonaComponent & JSXBase.HTMLAttributes<HTMLJonaComponentElement>;
+            "julia-component": LocalJSX.JuliaComponent & JSXBase.HTMLAttributes<HTMLJuliaComponentElement>;
             "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
         }
     }
